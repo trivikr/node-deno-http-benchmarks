43e181039a7f7dbc7ef960fc826dbdf33ef2e55f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2020-07-15 06:19:21 -0700

    fix: ensures deno returns required headers
    Ref: https://github.com/denoland/deno/blob/master/std/http/http_bench.ts

--- deno_http.ts
+++ deno_http.ts
@@ -2,8 +2,15 @@ import { serve } from "https://deno.land/std@0.61.0/http/server.ts";
 
 const port = 3000;
 const s = serve({ port });
+const body = new TextEncoder().encode("Hello World");
 
 console.log("deno_http listen on", port);
 for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
+  const res = {
+    body,
+    headers: new Headers(),
+  };
+  res.headers.set("Date", new Date().toUTCString());
+  res.headers.set("Connection", "keep-alive");
+  req.respond(res).catch(() => {});
 }
--- node_http.js
+++ node_http.js
@@ -3,10 +3,8 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.end("Helloi World!");
-});
-
-server.listen(port, hostname, () => {
+http.createServer((req, res) => {
+  res.end("Hello World");
+}).listen(port, hostname, () => {
   console.log("node listening on:", port);
 });
